2b249b3249eeffbc8c49ede152ca179ea5c67717
Gareth Ashton <gashton@flowdeck.com>
2024-04-14 21:24:39 -0700

    add User Model methods

--- src/db/users.ts
+++ src/db/users.ts
@@ -11,3 +11,19 @@ const UserSchema = new mongoose.Schema({
 })
 
 export const UserModel = mongoose.model('User', UserSchema)
+
+export const getUsers = () => UserModel.find()
+
+export const getUserByUsername = (username: string) => UserModel.findOne({ username })
+
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({
+    'authentication.sessiontToken': sessionToken,
+  })
+export const getUserById = (id: string) => UserModel.findById(id)
+
+export const createUser = (values: Record<string, any>) => new UserModel(values)
+
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id })
+
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values)
