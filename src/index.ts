78a1736e32d50fac2f7865b50e253650049f0655
Gareth Ashton <gashton@flowdeck.com>
2024-04-14 20:14:44 -0700

    install mongoose, initial connection code

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express()
 const port = 8090
@@ -16,7 +17,7 @@ app.use(compression())
 app.use(cookieParser())
 app.use(bodyParser.json())
 
-// Define the route for GET requests to "/api/users"
+// Define the route for GET requests to "/users"
 app.get('/users', (req, res) => {
     let users = ['user1', 'user2']
     res.json(users)
@@ -27,3 +28,19 @@ const server = http.createServer(app)
 server.listen(port, () => {
     console.log(`Server running on localhost:${port}`)
 })
+
+//* DB Config
+const dbConfig = {
+    HOST: '127.0.0.1',
+    PORT: 27017,
+    DB: 'flowdeck'
+}
+
+const MongoURL = `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
+
+mongoose.Promise = Promise
+mongoose.connect(MongoURL).then(() => {
+    console.log('Connected to MongoDB')
+})
+
+mongoose.connection.on('error', (error: Error) => console.log(error))
