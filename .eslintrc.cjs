92ef6178d0951405c320dcf3e5645ad933274b06
Gareth Ashton <gashton@flowdeck.com>
2024-04-14 20:57:48 -0700

    eslint and prettier configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: false, es2020: true, node: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    // 'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn', // Override to warning here
+  },
+  // overrides: [
+  //   {
+  //     files: ['*.ts', '*.tsx'], // Apply to all TypeScript files
+  //     rules: {
+  //       '@typescript-eslint/no-unused-vars': 'warn', // Override to warning here
+  //     },
+  //   },
+  // ],
+}
