86ee2e88ececb389c0aabbad0be2754ea5627da3
Gareth Ashton <gashton@flowdeck.com>
2024-04-15 21:01:51 -0700

    get secret from env file

---
+++ src/helpers/index.ts
@@ -0,0 +1,15 @@
+import crypto from 'crypto'
+
+export const randomUser = () => {
+  return process.env.SECRET
+}
+
+export const random = () => crypto.randomBytes(128).toString('base64')
+
+export const authentication = (salt: string, password: string) => {
+  if (!process.env.SECRET) {
+    throw new Error('Missing environment variable: SECRET')
+  }
+
+  return crypto.createHmac('sha256', [salt, password].join('/')).update(process.env.SECRET).digest('hex')
+}
