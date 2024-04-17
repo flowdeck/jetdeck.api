3f94e8fa7443581431d4475cac3495343319732a
Gareth Ashton <gashton@flowdeck.com>
2024-04-16 20:55:30 -0700

    users controller

---
+++ src/controllers/users.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import { getUsers } from '../db/users'
+
+export const getAllUsers = async (req: express.Request, res: express.Response) => {
+  const users = await getUsers()
+
+  return res.status(200).json({ users })
+}
