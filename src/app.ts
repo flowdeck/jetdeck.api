0c55384a462647b8d7724179451e42f115314469
Gareth Ashton <gashton@flowdeck.com>
2024-04-18 20:10:42 -0700

    empty server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello, World!')
+import express from 'express'
+
+//* Create a new express application instance
+const app = express()
+
+//* Default port
+const port = 8090
+
+app.listen(8090, () => {
+  console.log(`Server is running on port ${port}`)
+})
