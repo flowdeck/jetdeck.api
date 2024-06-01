2b1914c868b193c41206d7de13d1dd222dd11d1a
Gareth Ashton <gashton@flowdeck.com>
2024-06-01 14:01:00 -0700

    add MochError route

--- src/routes/airport.routes.js
+++ src/routes/airport.routes.js
@@ -3,7 +3,22 @@ import airportController from '../controllers/airport.controller.js'
 
 const router = Router()
 
-// router.get('/airports', (req, res) => airportController.getAirports(req, res))
+const returnMochError = true
+
+if (returnMochError) {
+  router.get('/airports', (req, res) => {
+    // Simulate an error scenario
+    const mockError = {
+      status: 500, // Internal Server Error (or another appropriate code)
+      message: 'Error fetching airports data',
+    }
+
+    res.status(mockError.status).json(mockError) // Return the error as JSON
+  })
+} else {
+  router.get('/airports', (req, res) => airportController.getAirports(req, res))
+}
+
 router.get(
   '/airports',
   (req, res) => airportController.getAirports(req, res, req.query.t), // Pass 't' parameter
